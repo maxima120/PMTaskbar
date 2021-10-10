8f3fafee55e2a9c4a03079186939ffe5c9d8aa03
Maxima <maxima120@hotmail.com>
2021-10-10 19:40:02 +0200

    can save links, position, size can resize height timer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,15 +26,129 @@ namespace PMTaskbar
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region ctor and fields
+
+        private readonly SettingsManager<UserSettings> settingsManager;
+        private readonly UserSettings settings;
+
+        Timer timer;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            items = new ObservableCollection<LinkItem>();
-            lst.ItemsSource = items;
+            settingsManager = new SettingsManager<UserSettings>("pmt.settings.json");
+            settings = settingsManager.LoadSettings();
+
+            ProcessSettings(settings);
+
+            StartWallClock();
         }
 
-        ObservableCollection<LinkItem> items;
+        #endregion
+
+        #region settings
+
+        private void ProcessSettings(UserSettings settings)
+        {
+            this.Top = settings.Top;
+            this.Left = settings.Left;
+            this.Height = settings.Height;
+
+            foreach (var link in settings.Links)
+            {
+                settings.items.Add(new LinkItem { link = link, imgSrc = GetIcon(link) });
+            }
+
+            lst.ItemsSource = settings.items;
+        }
+
+        #endregion
+
+        #region pinvokes
+
+        public ImageSource GetIcon(string fileName)
+        {
+            try
+            {
+                Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(fileName);
+
+                var imgSrc =
+
+                    System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                            icon.Handle,
+                            new Int32Rect(0, 0, icon.Width, icon.Height),
+                            BitmapSizeOptions.FromEmptyOptions());
+
+                return imgSrc;
+
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+        private const int GWL_STYLE = -16;
+
+        private const uint WS_SYSMENU = 0x80000;
+
+        #endregion
+
+        #region overrides
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            settings.Top = this.Top;
+            settings.Left = this.Left;
+            settings.Height = this.Height;
+            settingsManager.SaveSettings(settings);
+
+            base.OnClosing(e);
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE,
+                GetWindowLong(hwnd, GWL_STYLE) & (0xFFFFFFFF ^ WS_SYSMENU));
+
+            base.OnSourceInitialized(e);
+        }
+
+        #endregion
+
+        #region timer
+
+        private void StartWallClock()
+        {
+            this.TimeText.Text = DateTime.Now.ToString("HH:mm");
+            this.WeekdayText.Text = DateTime.Now.ToString("ddd");
+
+            timer = new Timer(1000);
+            timer.Start();
+            timer.Elapsed += (o, e) =>
+            {
+                this.Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    this.TimeText.Text = DateTime.Now.ToString("HH:mm");
+                    this.WeekdayText.Text = DateTime.Now.ToString("ddd");
+                })
+                );
+            };
+
+            //Text="{Binding Time, ElementName=clock, StringFormat=\{0:hh\\:mm\}, Mode=OneWay}"
+        }
+
+        #endregion
+
+        #region UI events
 
         private void ListView_Drop(object sender, DragEventArgs e)
         {
@@ -59,56 +175,10 @@ namespace PMTaskbar
                 if (!s.EndsWith(".lnk"))
                     continue;
 
-                Debug.WriteLine("GOT: " + s);
-
-                items.Add(new LinkItem { imgSrc = GetIcon(s), link = s });
+                settings.Links.Add(s);
+                settings.items.Add(new LinkItem { imgSrc = GetIcon(s), link = s });
+                settingsManager.SaveSettings(settings);
             }
-        }
-
-        private void ListView_MouseMove(object sender, MouseEventArgs e)
-        {
-            //Debug.WriteLine("{0}", e.LeftButton);
-        }
-
-        public ImageSource GetIcon(string fileName)
-        {
-            try
-            {
-                Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(fileName);
-
-                var imgSrc =
-
-                    System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
-                            icon.Handle,
-                            new Int32Rect(0, 0, icon.Width, icon.Height),
-                            BitmapSizeOptions.FromEmptyOptions());
-
-                return imgSrc;
-
-            }
-            catch (Exception)
-            {
-                return null;
-            }        
-        }
-
-        [DllImport("user32.dll")]
-        static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll")]
-        static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-
-        private const int GWL_STYLE = -16;
-
-        private const uint WS_SYSMENU = 0x80000;
-
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
-            SetWindowLong(hwnd, GWL_STYLE,
-                GetWindowLong(hwnd, GWL_STYLE) & (0xFFFFFFFF ^ WS_SYSMENU));
-
-            base.OnSourceInitialized(e);
         }
 
         private void lst_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -117,17 +187,28 @@ namespace PMTaskbar
             if (item == null)
                 return;
 
-            Debug.WriteLine("CLICK: " + item.link);
-
-            Process.Start(new ProcessStartInfo { FileName = item.link, UseShellExecute = true });;
+            // TODO : see if more needs to be done to avoid any dependency between this process and the children.
+            Process.Start(new ProcessStartInfo { FileName = item.link, UseShellExecute = true });
 
             lst.SelectedItem = null;
         }
-    }
 
-    public class LinkItem
-    {
-        public ImageSource imgSrc { get; set; }
-        public string link { get; set; }
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Height <= 2 * 60)
+                return;
+
+            this.Height -= 60;
+        }
+
+        private void DnButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Height >= 20 * 60)
+                return;
+
+            this.Height += 60;
+        }
+
+        #endregion
     }
 }
---
+++ SettingsManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PMTaskbar
+{
+    public class SettingsManager<T> where T : class, new()
+    {
+        private readonly string filePath;
+
+        public SettingsManager(string fileName)
+        {
+            filePath = GetLocalFilePath(fileName);
+            Trace.WriteLine(filePath);
+        }
+
+        private string GetLocalFilePath(string fileName)
+        {
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            return Path.Combine(appData, fileName);
+        }
+
+        public T LoadSettings()
+        {
+            if (File.Exists(filePath))
+                return JsonSerializer.Deserialize<T>(File.ReadAllText(filePath));
+
+            return new T();
+        }
+
+        public void SaveSettings(T settings)
+        {
+            string json = JsonSerializer.Serialize(settings);
+            File.WriteAllText(filePath, json);
+        }
+    }
+
+    /// <summary>
+    /// items holder for serialization
+    /// </summary>
+    public class UserSettings
+    {
+        public UserSettings()
+        {
+            items = new ObservableCollection<LinkItem>();
+            Links = new List<string>();
+            Top = 10;
+            Left = 10;
+            Height = 470;
+        }
+
+        [JsonIgnore]
+        public ObservableCollection<LinkItem> items { get; set; }
+
+        public List<string> Links { get; set; }
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Height { get; set; }
+    }
+
+    /// <summary>
+    /// UI data item
+    /// </summary>
+    public class LinkItem
+    {
+        public ImageSource imgSrc { get; set; }
+        public string link { get; set; }
+    }
+}
